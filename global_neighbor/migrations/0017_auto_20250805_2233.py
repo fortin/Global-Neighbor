a30ded3a81d267759384dc3fccf73c78cbcba659
Antonio Fortin <antonio@kikodo.io>
2025-08-06 00:37:02 +0200

    Finalize schema state for global_neighbor; clean up fake cms migration

---
+++ global_neighbor/migrations/0017_auto_20250805_2233.py
@@ -0,0 +1,13 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        (
+            "global_neighbor",
+            "0016_document_author_document_category_document_source_and_more",
+        ),
+        ("cms", "0041_alter_pageurl_unique_together_pageurl_site_and_more"),
+    ]
+
+    operations = []
